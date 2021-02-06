03cd30c815997e5d4ba3000c1be3694e246af3f4
Victor Morales <chipahuac@hotmail.com>
2021-02-05 18:20:36 -0800

    Add Concourse CI pipelines

--- build/ci/run_task.sh
+++ build/ci/run_task.sh
@@ -1,13 +1,24 @@
+#!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2020
+# Copyright (c)
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-.PHONY: build
-build:
-	@docker build -t electrocucaracha/linter .
-	@docker image prune --force
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o xtrace
+
+if ! command -v fly; then
+    # NOTE: Shorten link -> https://github.com/electrocucaracha/pkg-mgr_scripts
+    curl -fsSL http://bit.ly/install_pkg | PKG=fly bash
+fi
+
+fly execute --privileged \
+    --target="${RELENG_TARGET:-releng}" \
+    --config="${TASK_NAME}-task.yml" \
+    -i src=../../ "$@"
